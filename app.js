3ad4a88c3f4b4d44c4e72fe3fbee02078b90d477
katyvonkaty <kj0203@gmail.com>
2020-09-18 15:41:04 -0400

    updated

--- app.js
+++ app.js
@@ -1,31 +1,60 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+})
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User", userSchema)
 
 app.get("/", function(req, res) {
   res.render("index")
+})
+
+
+
+app.get("/shop", function(req, res) {
+  res.render("shop")
+})
+
+app.get("/reviews", function(req, res) {
+  res.render("reviews")
 })
 
 app.get("/login", function(req, res) {
   res.render("login")
 })
 
-app.get("/shop", function(req, res) {
-  res.render("shop")
-})
-
 app.post("/login", function(req, res) {
-    res.redirect("/shop")
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.render("shop")
+    }
+  });
 });
 
 
-  app.listen(process.env.PORT || 3000, function() {
-    console.log("all systems go")
-  })
+app.listen(process.env.PORT || 3000, function() {
+  console.log("all systems go")
+})
