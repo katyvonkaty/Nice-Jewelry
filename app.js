62a3823d407967070d58a3803217f62c4bdb87c6
katyvonkaty <kj0203@gmail.com>
2020-09-19 18:17:05 -0400

    updated

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 3
 
 const app = express();
 
@@ -26,6 +27,7 @@ const userSchema = new mongoose.Schema({
 
 
 const User = new mongoose.model("User", userSchema)
+
 
 app.get("/", function(req, res) {
   res.render("index")
@@ -61,40 +63,47 @@ app.get("/register", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.email,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render("reviews")
-    }
-  });
+
+
+  bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+    const newUser = new User({
+      email: req.body.email,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render("reviews", {username: newUser.email})
+      }
+    });
+  })
+
+
 });
 
 app.post("/login", function(req, res) {
   const email = req.body.email;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: email }, function(err, foundUser){
     if (err) {
       console.log(err)
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("reviews")
-        } else {
-          res.redirect("404")
-          console.log(foundUser.password)
-        }
       } else {
-        res.redirect("404");
-      }
-    }
-  });
+          if (foundUser) {
+              bcrypt.compare(password, foundUser.password, function(err,result){
+                  if(result === true) {
+                  res.render("reviews", {username: email});
+                  } else {
+                  res.redirect("404")
+                  console.log(foundUser.password)
+                }
+            });
+      };
+    };
+  })
 });
 
 
