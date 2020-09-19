c978e38ca13727bfb4c32b95c162c0f00a455f86
katyvonkaty <kj0203@gmail.com>
2020-09-19 09:49:27 -0400

    updated

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
-
 
 
 app.use(express.static("public"));
@@ -26,9 +24,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET}, {
-  encryptedFields: ["password"]
-})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -68,7 +63,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.email,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -82,7 +77,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const email = req.body.email;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: email }, function(err, foundUser){
     if (err) {
