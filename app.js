b597e61e0a042de35115646e7c99fa8dd614f86a
katyvonkaty <kj0203@gmail.com>
2020-09-19 18:29:43 -0400

    updated

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 3
+
+// keep
+// const bcrypt = require("bcrypt");
+// const saltRounds = 3;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
-
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+app.use(session({
+  secret:"cockapoos are my life blood",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
@@ -23,8 +36,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-
 
 const User = new mongoose.model("User", userSchema)
 
@@ -63,10 +74,7 @@ app.get("/register", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-
-
-
-  bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
       email: req.body.email,
       password: hash
@@ -76,31 +84,35 @@ app.post("/register", function(req, res) {
       if (err) {
         console.log(err)
       } else {
-        res.render("reviews", {username: newUser.email})
+        res.render("reviews", {
+          username: newUser.email
+        })
       }
     });
   })
-
-
 });
 
 app.post("/login", function(req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ email: email }, function(err, foundUser){
+  User.findOne({
+    email: email
+  }, function(err, foundUser) {
     if (err) {
       console.log(err)
-      } else {
-          if (foundUser) {
-              bcrypt.compare(password, foundUser.password, function(err,result){
-                  if(result === true) {
-                  res.render("reviews", {username: email});
-                  } else {
-                  res.redirect("404")
-                  console.log(foundUser.password)
-                }
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("reviews", {
+              username: email
             });
+          } else {
+            res.redirect("404")
+            console.log(foundUser.password)
+          }
+        });
       };
     };
   })
