8d2de99955299af39ec7894fd7cc582bbf45113f
katyvonkaty <kj0203@gmail.com>
2020-09-20 11:27:39 -0400

    updated

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-// keep
-// const bcrypt = require("bcrypt");
-// const saltRounds = 3;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
 
 const app = express();
 
@@ -31,18 +30,63 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 })
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
-const User = new mongoose.model("User", userSchema)
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/reviews",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req, res) {
   res.render("index")
 })
+
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ["profile"] })
+)
+
+app.get("/auth/google/reviews",
+    passport.authenticate( 'google', {
+      failureRedirect: '/login' }),
+      function(req,res) {
+        res.redirect('/reviews')
+      }
+);
+
 
 app.get("/about", function(req, res) {
   res.render("about")
@@ -60,11 +104,6 @@ app.get("/404", function(req, res) {
   res.render("404")
 })
 
-
-app.get("/reviews", function(req, res) {
-  res.render("reviews")
-})
-
 app.get("/login", function(req, res) {
   res.render("login")
 })
@@ -73,50 +112,76 @@ app.get("/register", function(req, res) {
   res.render("register")
 })
 
-app.post("/register", function(req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.email,
-      password: hash
-    });
+app.get("/reviews", function(req, res) {
+    if(req.isAuthenticated()) {
+      res.render("reviews")
+    } else {
+      res.redirect("/login")
+    }
+})
 
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.render("reviews", {
-          username: newUser.email
-        })
-      }
-    });
+// app.post("/register", function(req, res) {
+//   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//     const newUser = new User({
+//       email: req.body.email,
+//       password: hash
+//     });
+//
+//     newUser.save(function(err) {
+//       if (err) {
+//         console.log(err)
+//       } else {
+//         res.render("reviews", {
+//           username: newUser.email
+//         })
+//       }
+//     });
+//   })
+// });
+
+app.get("/submit", function(req,res){
+
+})
+
+app.get("/logout", function(req,res) {
+  req.logout();
+  res.redirect("/")
+})
+
+app.post("/register", function(req,res){
+  User.register({username:req.body.username}, req.body.password, function(err,user) {
+    if(err){
+      console.log(err);
+      res.redirect("/register")
+    } else {
+      passport.authenticate("local") (req,res, function(){
+        res.redirect("/reviews")
+
+      })
+    }
   })
-});
+})
 
-app.post("/login", function(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  User.findOne({
-    email: email
-  }, function(err, foundUser) {
-    if (err) {
+
+app.post("/login", function(req,res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.passport
+  })
+
+  req.login(user, function(err) {
+    if(err) {
       console.log(err)
     } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("reviews", {
-              username: email
-            });
-          } else {
-            res.redirect("404")
-            console.log(foundUser.password)
-          }
-        });
-      };
-    };
+      passport.authenticate("local") (req,res,function(){
+        res.render("reviews", {username: user.username})
+      })
+    }
   })
-});
+})
+
+// TODO: should be redirect but trying to surface username
 
 
 app.listen(process.env.PORT || 3000, function() {
